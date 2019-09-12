a7e5bd49aacf12e4e0f069a37b6d35f744c61189
Sharrieff ross <crross@dons.usfca.edu>
2019-09-12 21:06:02 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, next_color)
+  array.push(next_color)
+end
+
+def using_unshift(array, new_neighborhood)
+  array.unshift(new_neighborhood)
+end
+  
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array)
+  [array].concat([array])
+end
